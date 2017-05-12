5fd066114db9a0f9c6739b15db443275accffc6b
Manami Ichikawa <mana@Ms-MacBook-Pro.local>
2017-05-12 17:13:23 +0900

    first commit

---
+++ ios-foundation-dictionary-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-foundation-dictionary-demo
+//
+//  Created by Manami Ichikawa on 5/12/17.
+//  Copyright © 2017 Manami Ichikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
